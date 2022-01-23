cb9892f4e534b29e4f6ad6c49ec7908bf2858342
Etienne M <em85@pm.me>
2022-01-24 00:09:27 +0100

    change homescreen

--- index.tsx
+++ index.tsx
@@ -1,39 +1,51 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { ThreeReactWrapper } from '../pwa-tools/tools/ThreeReactWrapper';
-import { AppRoutes, NavBar } from "../pwa-tools/utils/routing"
-
 import { ForkniteDemo } from "./forknite-demo/ForkniteDemo"
-import { LoadingScreen } from "../pwa-tools/tools/LoadingScreen"
+// import { AppRoutes, NavBar } from "../pwa-tools/utils/routing"
 
 import { BASENAME } from './config';
 import "./styles.css"
+import "../pwa-tools/pwa.css"
 
 // Entry point
 const App = () => {
     return (
         <BrowserRouter basename={BASENAME}>
             <Routes >
-                <Route path="/*" element={<Entries />} />
+                <Route index element={<Home />} />
+                <Route path="/forknite" element={<ThreeReactWrapper appClass={ForkniteDemo} />} />
             </Routes>
         </BrowserRouter>
     );
 }
 
+const Home = () => {
+    return (<div className="HomeScreen ">
+        <div className="center">
+            {/* <img src={logo} className="logo" alt="logo" /> */}
+            <h2>
+                WELCOME
+            </h2>
+            <Link to={"/forknite"}> <p>Press to start</p> </Link>
+        </div>
+    </div>)
+}
+
 /**
  * Index all apps here
  */
-const Entries = () => {
-    return (
-        <div className="App">
-            <NavBar />
-            <div className="AppItems">
-                <AppRoutes style={{ position: "absolute" }}>
-                    <LoadingScreen />
-                    <ThreeReactWrapper appClass={ForkniteDemo} />
-                </AppRoutes>
-            </div>
-        </div>
-    )
-}
+// const Entries = () => {
+//     return (
+//         <div className="App">
+//             <NavBar />
+//             <div className="AppItems">
+//                 <AppRoutes style={{ position: "absolute" }}>
+//                     <LoadingScreen />
+//                     <ThreeReactWrapper appClass={ForkniteDemo} />
+//                 </AppRoutes>
+//             </div>
+//         </div>
+//     )
+// }
 
 export default App
